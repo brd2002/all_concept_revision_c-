b2f3a9121b52e2e496ae22048b0e41886a60c680
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-05 18:59:19 +0530

    print all the node  using while loop

--- learn_linkedList.cpp
+++ learn_linkedList.cpp
@@ -2,18 +2,41 @@
 using namespace std;
 class Node
 {
-    public:
+public:
     int data;
     Node *next;
     Node(int value)
-    {data = value;
-    next = NULL;
+    {
+        data = value;
+        next = NULL;
     }
     // node type address
 };
+void printAllNode (Node * head){
+    while(head != NULL) { 
+        cout << head -> data << endl;
+        head = head->next;
+    }
+}
+
 int main()
 {
     Node *head;
     head = new Node(4);
+    int arr[] = {1,2,3};
+    for (int i = 0 ; i < 3 ; i++){
+        if (head == NULL){
+            Node *head = new Node (arr[i]);
+        }else{
+            Node *temp = new Node (arr[i]);
+            temp->next = head;
+            head = temp;
+        }
+    }
+    printAllNode(head) ;
+    // print the head data
+    // cout << head->data << endl;
+    // cout << head->next << endl;
+
     return 0;
 }
