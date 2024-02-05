58a0f12732b4222c163cca6d9900ac5a07dcbe4a
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-05 19:20:48 +0530

    last element added in last node

--- learn_linkedList.cpp
+++ learn_linkedList.cpp
@@ -12,9 +12,21 @@ public:
     }
     // node type address
 };
-void printAllNode (Node * head){
-    while(head != NULL) { 
-        cout << head -> data << endl;
+void addInEnd(Node *head, int data)
+{
+    Node * tail ;
+    tail = head;
+    while (tail->next != NULL){
+        tail = tail->next ;
+    }
+    Node *temp = new Node(data);
+    tail->next = temp;
+}
+void printAllNode(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << endl;
         head = head->next;
     }
 }
@@ -23,17 +35,22 @@ int main()
 {
     Node *head;
     head = new Node(4);
-    int arr[] = {1,2,3};
-    for (int i = 0 ; i < 3 ; i++){
-        if (head == NULL){
-            Node *head = new Node (arr[i]);
-        }else{
-            Node *temp = new Node (arr[i]);
+    int arr[] = {1, 2, 3};
+    for (int i = 0; i < 3; i++)
+    {
+        if (head == NULL)
+        {
+            Node *head = new Node(arr[i]);
+        }
+        else
+        {
+            Node *temp = new Node(arr[i]);
             temp->next = head;
             head = temp;
         }
     }
-    printAllNode(head) ;
+    addInEnd(head , 12);
+    printAllNode(head);
     // print the head data
     // cout << head->data << endl;
     // cout << head->next << endl;
