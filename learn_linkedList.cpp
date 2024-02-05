5d94f8aba21f1266b4cdcf137dc91b64c377103a
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-05 13:13:47 +0530

    first create node in linked list

---
+++ learn_linkedList.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    Node(int value)
+    {data = value;
+    next = NULL;
+    }
+    // node type address
+};
+int main()
+{
+    Node *head;
+    head = new Node(4);
+    return 0;
+}
